ffbfa075b3ebe94603912ad210ec63062b602d34
huanghai <1165441461@qq.com>
2018-03-23 17:40:31 +0800

    勿忘初心

---
+++ app/src/main/java/com/myhuanghai/mvvmdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.myhuanghai.mvvmdemo;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myhuanghai.mvvmdemo.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        List<String> list = new ArrayList<>();
+        list.add("list1");
+        list.add("list2");
+        binding.setList(list);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("key0", "map_value0");
+        map.put("key1", "map_value1");
+        binding.setMap(map);
+
+        String[] arrays = {"字符串1", "字符串2"};
+        binding.setArray(arrays);
+
+
+    }
+}
